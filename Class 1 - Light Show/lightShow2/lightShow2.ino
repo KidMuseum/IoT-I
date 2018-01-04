d448125e5df3990646550a9dd9476e15ee5f7f30
KidMuseum <elvin@kid-museum.org>
2018-01-04 09:20:12 -0500

    Second Commit

---
+++ Class 1 - Light Show/lightShow2/lightShow2.ino
@@ -0,0 +1,33 @@
+const int ledPin = 13;
+const int led2Pin = 12;
+const int led3Pin = 11;
+
+const int buttonPin = 2;
+bool buttonState ;
+
+void setup() {
+  pinMode(buttonPin = INPUT);
+  pinMode(ledPin = OUTPUT);
+}
+void loop() {
+  buttonState = digitalRead(buttonPin);  
+  if (buttonState == HIGH)  {
+    doLightShow;  }
+    else{
+     digitalWrite(ledPin,LOW);
+   }
+ }
+   void doLightShow{
+     digitalWrite(ledPin,HIGH);
+     delay(300);
+     digitalWrite(led2Pin,HIGH);
+     delay(300);
+     digitalWrite(led3Pin,HIGH);
+     delay(300);
+     digitalWrite(led3Pin,LOW);
+     delay(300);
+     digitalWrite(led2Pin,LOW);
+     delay(300);
+     digitalWrite(ledPin,LOW);
+     delay(300);
+   }
